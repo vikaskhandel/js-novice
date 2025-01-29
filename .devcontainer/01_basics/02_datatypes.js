ed68d61433b59aa7e645ddc51ccd6dd203c57ebe
Vikas Khandel <vikaskhandel@users.noreply.github.com>
2025-01-29 18:08:04 +0000

    Datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"user strict"; //treat all JS code as newer version
+
+// alert(3+3) // We are using node js and not browser
+
+// code readability is important
+
+console.log(3+3);
+console.log("vikas");
+
+let age = 33
+let name = "Vikas"
+let isLoggedin = false
+
+// Primitive DataTypes //
+// number 2 to the power 53
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+// Objects
+
+console.log(typeof undefined)
