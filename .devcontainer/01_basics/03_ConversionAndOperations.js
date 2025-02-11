1f27c14faff3095e994af8cbdb40adf563ac8143
Vikas Khandel <vikaskhandel@users.noreply.github.com>
2025-02-11 17:57:31 +0000

    Conversion and Operations Part 1

---
+++ .devcontainer/01_basics/03_ConversionAndOperations.js
@@ -0,0 +1,31 @@
+let score = "33aa"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let ValueInNumber = Number(score)
+
+//console.log(ValueInNumber)
+//console.log(typeof ValueInNumber)
+
+// "33" => 33
+// "33sds" => Nan
+// true => 1 false => 0
+
+let IsBooleanValue = ""
+
+let IsBoolean = Boolean(IsBooleanValue)
+
+//console.log(IsBoolean);
+//console.log(typeof IsBoolean);
+
+// 1 => true, 0 => false
+// "" => false
+// "vikas" => true
+
+let SomeNumber = 44
+
+let StringToNumber = String(SomeNumber)
+
+console.log(StringToNumber);
+console.log(typeof StringToNumber);
