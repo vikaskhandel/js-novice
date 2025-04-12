6314e4041de84363ef32071d93f201744b3a0bc1
Vikas Khandel <vikaskhandel@users.noreply.github.com>
2025-04-12 14:43:10 +0000

    Strings Started

---
+++ .devcontainer/01_basics/Strings.js
@@ -0,0 +1,8 @@
+const name = "VikasRepo"
+const RepoCount = 50
+
+// Below is Outdated method
+console.log(name + RepoCount + " Value");
+
+// Below is String interpolation and is a modern method to write console logs
+console.log(`Hello My name is ${name} and my Rep Count is ${RepoCount}`);
