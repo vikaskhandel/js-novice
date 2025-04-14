8f0188418f83f67c877cce91b683008e256812b1
Vikas Khandel <vikaskhandel@users.noreply.github.com>
2025-04-14 18:52:11 +0000

    MathAndNum

---
+++ .devcontainer/01_basics/NumberAndMath.js
@@ -0,0 +1,44 @@
+// const score = 400
+// console.log(score);
+
+
+// const balance = new Number(100.333)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(2))
+
+// const othernum = 123.33
+// console.log(othernum.toPrecision(3));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString()); // US style representation
+
+// console.log(hundreds.toLocaleString('en-IN')); // Indian style representation
+
+// ++++++++++++++++ Maths ++++++++++++++++++++++
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+
+// console.log(Math.sqrt(25));
+// console.log(Math.min(4,54,1,67,77));
+// console.log(Math.max(4,54,1,67,77));
+
+console.log(Math.random()); // Values always between 0 and 1
+
+console.log((Math.random()*10) + 1);
+
+// Values needed between 10 and 20
+
+ const min = 10
+ const max = 20
+
+ console.log(Math.floor((Math.random() * (max - min + 1))) + min);
+ 
