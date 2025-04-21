bc7ff4600fb91c1409b37ff6ae3a9646b911a5a4
Vikas Khandel <vikaskhandel@users.noreply.github.com>
2025-04-21 18:00:01 +0000

    Arrays Rename and Part 2

---
+++ .devcontainer/02_basics/02_Arrays.js
@@ -0,0 +1,10 @@
+const mynewArr = [1,2,3,4]
+const mynewArr1 = [5,6,7,8]
+
+console.log(mynewArr.concat(mynewArr1))
+
+// console.log(mynewArr.push(mynewArr1))
+// console.log(mynewArr);
+
+const newarr = mynewArr.concat(mynewArr1)
+console.log(newarr);
